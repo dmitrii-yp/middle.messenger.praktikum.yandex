9e560b24dd2ad94fa963fd6390a87e0b8a3c30a3
dmitrii-yp <da.mcarov@ya.ru>
2023-03-17 19:45:07 +0300

    add title to chat top bar

--- src/components/chat/topbar.ts
+++ src/components/chat/topbar.ts
@@ -1,9 +1,25 @@
 import Block from '../../core/block';
 import templateString from 'bundle-text:./topbar.hbs';
+import { Chat } from '../../typings/api-types';
 
-export class ChatTopBar extends Block {
+interface ChatTopBarProps {
+  chats: {
+    data: Chat;
+    activeChatId: number;
+  };
+  title: string;
+}
+
+export class ChatTopBar extends Block<ChatTopBarProps> {
   constructor(props: any) {
     super(props);
+    const title = props.chats.data.find(
+      (chat: Chat) => chat.id === props.chats.activeChatId
+    )?.title;
+
+    if (title) {
+      this.setProps({ title });
+    }
   }
 
   static get componentName() {
