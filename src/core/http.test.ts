440abc5a6383a6f3625180149713b54a920801d3
dmitrii-yp <da.mcarov@ya.ru>
2023-04-01 12:25:52 +0300

    finalize http tests

--- src/core/http.test.ts
+++ src/core/http.test.ts
@@ -3,9 +3,10 @@ import { HTTP } from './http';
 import { expect } from 'chai';
 
 describe('HTTP trasnport', () => {
-  let requests: sinon.SinonFakeXMLHttpRequest[] = [];
   const originalXHR = global.XMLHttpRequest;
-
+  const url = '/';
+  let requests: sinon.SinonFakeXMLHttpRequest[] = [];
+  let http: HTTP;
 
   before(() => {
     const XHR = sinon.useFakeXMLHttpRequest();
@@ -16,6 +17,8 @@ describe('HTTP trasnport', () => {
     XHR.onCreate = (xhr) => {
       requests.push(xhr);
     };
+
+    http = new HTTP(url);
   });
 
   beforeEach(() => {
@@ -28,14 +31,57 @@ describe('HTTP trasnport', () => {
 
   describe('GET', () => {
     it('should make GET request', () => {
-      const http = new HTTP('/');
-      http.get('/');
+      http.get(url);
 
       expect(requests[0].method).to.eq('GET');
     });
   });
 
-  // describe('POST', () => {
+  describe('POST', () => {
+    it('should make POST request', () => {
+      http.post(url);
 
-  // });
+      expect(requests[0].method).to.eq('POST');
+    });
+
+    it('should include data in POST request', () => {
+      const testData = { test: 'test' };
+      http.post(url, { data: testData });
+
+      expect(requests[0].requestBody).to.eq(JSON.stringify(testData));
+    });
+  });
+
+  describe('PUT', () => {
+    it('should make PUT request', () => {
+      http.put(url);
+
+      expect(requests[0].method).to.eq('PUT');
+    });
+
+    it('should send FormData in PUT request', () => {
+      const formData = new FormData();
+      formData.append('test', 'test');
+      http.put(url, { data: formData });
+
+      expect(requests[0].requestBody).to.be.an.instanceof(FormData);
+    });
+
+    it('should not add ContentType header to FormData in PUT request', () => {
+      const formData = new FormData();
+      formData.append('test', 'test');
+      http.put(url, { data: formData });
+
+      expect(requests[0].requestHeaders).to.be.an.instanceof(Object);
+      expect(requests[0].requestHeaders).to.be.empty;
+    });
+  });
+
+  describe('DELETE', () => {
+    it('should make DELETE request', () => {
+      http.delete(url);
+
+      expect(requests[0].method).to.eq('DELETE');
+    });
+  });
 });
